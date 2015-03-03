60618deccac83000a03a5ab9fb61fa4f18adcafe
twosnail <twosnail.sys@gmail.com>
2015-03-03 17:10:04 +0800

    基础项目初始化，反射

---
+++ src/main/java/com/twosnail/JavaBasicProject/reflect/Demo01.java
@@ -0,0 +1,23 @@
+package com.twosnail.JavaBasicProject.reflect;
+/**
+ * <b>Description:</b></br> 
+ * <b>Title:</b>通过一个对象获得完整的包名和类名
+ * @company:7dosoft
+ * @author: txd
+ * @Date: 2015年3月3日下午5:02:22
+ * @version 1.0
+ */
+public class Demo01 {
+
+	public static void main(String[] args) {
+		 Demo demo=new Demo();
+	     System.out.println(demo.getClass().getName());
+    }
+	
+}
+
+
+//添加一句：所有类的对象其实都是Class的实例。
+class Demo{
+    
+}
